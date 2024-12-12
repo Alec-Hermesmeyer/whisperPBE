1327335bf09470be5250e27e151378038549536d
Alec Hermesmeyer <ahrmsmyr@gmail.com>
2024-12-12 15:59:03 -0600

    commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from whisper import whisper
+import whisper
 import torch
 from werkzeug.utils import secure_filename
 
